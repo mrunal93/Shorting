b5529c73939444d42d642ff8a6b70ab36db23432
mrn26893@gmail.com <mrn26893@gmail.com>
2020-06-12 23:06:24 +0530

    Equation 3

--- shorting.sh
+++ shorting.sh
@@ -6,5 +6,6 @@ read -p "Enter the 2nd Number" b
 read -p "Enter the 3rd number" c
 
 eqa1=$(($((a + b)) * c ))
-eqa2=$(($(($a * $b)) + $c ))
+eqa2=$(($(( $a * $b)) + $c ))
+eqa3=$(($(( $c + $a)) / $b ))
 
